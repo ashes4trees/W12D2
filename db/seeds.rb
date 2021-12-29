9bb3036cf0a1b3dc37e6a45e0b4eb9a5e310ab13
ashes4trees <stephsoni18@gmail.com>
2021-12-29 11:14:46 -0500

    complete part 2 phase 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bench.destroy_all
+
+bench1 = Bench.create(description: 'Beautiful bench', lat: 37.75782067231961, lng: -122.46764944683073)
+bench2 = Bench.create(description: 'Lots of shade', lat: 37.76840580284156, lng: -122.45340155396472)
+bench3 = Bench.create(description: 'Too hard', lat: 37.79391245516268, lng: -122.45752142660065)
+bench4 = Bench.create(description: 'Great place to feed pigeons', lat: 37.76894858921239, lng: -122.46541784915291)
+bench5 = Bench.create(description: 'Best people watching', lat: 37.76202776455821, lng: -122.4146060866428)
--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import configureStore from './store/store';
 import Root from './components/root';
 import { login } from './actions/session_actions';
+import { fetchBenches } from './actions/bench_actions';
 
 
 
@@ -29,5 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     window.login = login;
+    window.fetchBenches = fetchBenches;
     
 });
